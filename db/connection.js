34fac8a0eeabbc67922090793c0ff0dcfea1d650
ocampoad <ocampo.adriane13@gmail.com>
2022-07-16 10:58:59 -0700

    update db cred for production

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,25 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+require('dotenv').config();
+const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: "localhost",
+
+let connection;
+
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  }).promise();
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'todos_db',
-}).promise();
+  }).promise();
+}
 
-module.exports = connection; 
+
+module.exports = connection;
